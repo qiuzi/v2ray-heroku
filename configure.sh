cc527f68e274d5464bc9a1621757bbbefc27d169
qiuzi <gxfclql@gmail.com>
2021-01-05 14:32:05 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,11 +3,11 @@
 # Download and install V2Ray
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/p4gefau1t/trojan-go/releases/download/v0.8.2/trojan-go-linux-amd64.zip
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/wtrojan.zip https://github.com/qiuzi/v2ray-heroku/raw/master/wtrojan.zip
+#curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/wtrojan.zip https://github.com/qiuzi/v2ray-heroku/raw/master/wtrojan.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-unzip /tmp/v2ray/wtrojan.zip -d /tmp/v2ray
+#unzip /tmp/v2ray/wtrojan.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/trojan-go /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/wtrojan /usr/local/bin/wtrojan
+#install -m 755 /tmp/v2ray/wtrojan /usr/local/bin/wtrojan
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
@@ -36,17 +36,4 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 }
 EOF
 
-cat << EOF > /usr/local/etc/v2ray/wg0.conf
-[Interface]
-PrivateKey = MASsnLzQ3TSD/TUAYdJJC9S/eVjFZMphSck8j+0/wXQ=
-Address = fd01:5ca1:ab1e:8e09:3a79:e6ae:f988:13e3/128
-MTU = 1280
-[Peer]
-PublicKey = bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=
-AllowedIPs = ::/0
-Endpoint = engage.cloudflareclient.com:2408
-EOF
-
-# Run V2Ray
-/usr/local/bin/wtrojan wg0
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
